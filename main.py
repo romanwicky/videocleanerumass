c25923a47af38710f4783f56bcf6a0c8c2d0ff7f
Roman Wicky van Doyer <rwickyvd@gmail.com>
2021-01-08 11:58:13 -0500

    Update main.py
    final version v1

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 import cv2
 import os
 import glob
+import shutil
 import numpy as np
+
+# ** IMPORTANT NOTICE **
+# Please run this program as administrator, otherwise the cleanup process will not be able to delete the remaining files
 
 # Program Description
 # This program will take a video file as an input, and will output a trimmed version of the file
@@ -10,9 +14,6 @@ import numpy as np
 # The frames that need to be deleted are one's with no footage of the recorded tests
 
 # Algorithm Description
-
-# Algo 1
-
 # This algorithm will iterate through each frame of the video
 # Ex: Frame 1-3 need to be kept, frame 4-6 needs to be skipped, frame 7-8 kept
 # Frame 1 - Frame 2 = Difference, keep frame 1
@@ -30,7 +31,7 @@ import numpy as np
 
 # This one is much simpler, since we know for sure that a if a certain area of the screen
 # is Completly black, then that is a frame we want to discard, hence the "No picture" seconds
-# of the video. 
+# of the video.
 # Open file with cv2, crop it to [250:300, 250:300] [y1, y2], [x1, x2] where
 # (x1,y1) is the top left of the image, the (x2,y2) is the bottom right
 # By cropping and getting this specific area, and summing up the pixel values of the image
@@ -41,7 +42,7 @@ import numpy as np
 
 def convertVideoToImages():
     # eventually, user will be able to select multiple videos at once to process
-    cap = cv2.VideoCapture('C:/Users/Roman/Desktop/videos/vid1.avi')
+    cap = cv2.VideoCapture('C:/Users/Roman/Desktop/videos/trial7.avi')
     path = 'C:/Users/Roman/Desktop/videos/frames'
     i = 0
     # save each frame to its corresponding folder
@@ -61,7 +62,6 @@ def removeUnwantedFrames():
     # set i = number of frames
     listofframes = os.listdir(path)  # dir is your directory path
     count = len(listofframes)
-    # print(count)
     # list of all frames that need to be deleted
     framestodelete = []
 
@@ -77,24 +77,64 @@ def removeUnwantedFrames():
     # delete all frames from list
     for i in range(0, len(framestodelete)):
         os.remove('C:/Users/Roman/Desktop/videos/frames/' + str(framestodelete[i]) + '.jpg')
-        print(str(framestodelete[i]))
 
 
 def makeVideo():
     # take all cleaned up frames and make into video
     frame_array = []
-    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/*.jpg'):
+
+    # Because of the way glob.glob works, need to have separate for looks for frame names
+    # Here, assuming that the program won't exceed 999,999 total frames
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame?.jpg'):
         img = cv2.imread(filename)
         height, width, layers = img.shape
         size = (width, height)
         frame_array.append(img)
 
-    output = cv2.VideoWriter('C:/Users/Roman/Desktop/videos/frames/trimmed.avi', cv2.VideoWriter_fourcc(*'DIVX'), 20, size)
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame??.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(img)
+
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame???.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(img)
+
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame????.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(img)
+
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame?????.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(img)
+
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame??????.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(img)
+
+    for filename in glob.glob('C:/Users/Roman/Desktop/videos/frames/frame???????.jpg'):
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(img)
+
+    output = cv2.VideoWriter('C:/Users/Roman/Desktop/videos/trial7trimmed.avi', cv2.VideoWriter_fourcc(*'DIVX'), 29.97,
+                             size)
 
     for i in range(len(frame_array)):
         output.write(frame_array[i])
 
     output.release()
+    shutil.rmtree('C:/Users/Roman/Desktop/videos/frames', ignore_errors=False, onerror=None)
 
 
 def main():
@@ -115,6 +155,5 @@ def main():
     print("Exporting into video, please don't halt the program until process is complete")
     makeVideo()
     print("Process Finished")
-
 
 main()
